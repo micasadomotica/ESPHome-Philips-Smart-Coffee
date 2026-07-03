c454f5ee8840db1ab957e8cc4a1201ecfb3f28c8
Miguel <raidr1@gmail.com>
2026-07-03 22:44:21 +0200

    Fix inverted EP3321 display power restore

--- components/philips_coffee_machine/switch/power.cpp
+++ components/philips_coffee_machine/switch/power.cpp
@@ -32,15 +32,12 @@ namespace esphome
                         }
 
                         // Start power trip - cut power to display
-                        bool initial = *initial_state_;
+                        bool powered_value = *initial_state_;
                         ESP_LOGD(TAG, "Power trip %d starting - initial_state: %d, invert_power_pin: %d", 
-                                 power_trip_count_ + 1, initial, invert_power_pin_);
-                        if (invert_power_pin_) {
-                            initial = !initial;
-                        }
-                        bool trip_value = !initial;
+                                 power_trip_count_ + 1, powered_value, invert_power_pin_);
+                        bool trip_value = !powered_value;
                         ESP_LOGD(TAG, "Cutting power to display - setting pin from %d to %d (goal: cut power)", 
-                                 initial, trip_value);
+                                 powered_value, trip_value);
                         power_pin_->digital_write(trip_value);
                         power_trip_active_ = true;
                         power_trip_start_time_ = now;
@@ -51,9 +48,6 @@ namespace esphome
                     {
                         // Restore power to display
                         bool restore_value = *initial_state_;
-                        if (invert_power_pin_) {
-                            restore_value = !restore_value;
-                        }
                         ESP_LOGD(TAG, "Restoring power to display - setting pin to %d (goal: restore power)", 
                                  restore_value);
                         power_pin_->digital_write(restore_value);
